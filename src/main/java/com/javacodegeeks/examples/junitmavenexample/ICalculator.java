4d96b68e80c23ff5e37e26304b50246ea8376137
KCHAOU <kchaoucss@gmail.com>
2018-06-12 14:13:54 +0200

    nouvelle correction je termine prob53

--- src/main/java/com/javacodegeeks/examples/junitmavenexample/ICalculator.java
+++ src/main/java/com/javacodegeeks/examples/junitmavenexample/ICalculator.java
@@ -2,11 +2,12 @@ package com.javacodegeeks.examples.junitmavenexample;
 
 public interface ICalculator {
 	
-	//sum je termine mes travaux
+	
 	int sum(int a, int b);
 
 	int subtraction(int a, int b) throws Exception;
-
+	
+	//sum je termine mes travaux prob53
 	int multiplication(int a, int b);
 
 	int divison(int a, int b) throws Exception;
